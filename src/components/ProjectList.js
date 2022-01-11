2b764a84887102c0c36af75016d2a96ec6c8cd88
Francesca Hamm <francesca.c.hamm@gmail.com>
2022-01-11 00:08:10 -0700

    commit tree f801e08131c2a2c7400e0a746e4152eabf7b0a23

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,18 @@
 import React from "react";
+//import user from "../data/user"
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+ const info = projects.map(({name, about, id}) => 
+  <ProjectItem key={id} name={name} about={about} />
+ )
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {info}
+      </div>
     </div>
   );
 }
